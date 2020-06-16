3e38623ff47513a00e676f79cf7ee75dd5399581
Jonathan Theodoric <theodoricjonathan@gmail.com>
2020-06-16 10:42:23 +0700

    Added payment via history in progress feature

---
+++ src/main/java/demo/locator/HomePageLocator.java
@@ -0,0 +1,16 @@
+package demo.locator;
+
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+
+public interface HomePageLocator {
+    By DANA_Balance = MobileBy.id("tvBalance");
+    By Home_Button = MobileBy.id("menuHome");
+    By MobileRecharge_Button = MobileBy.id("btnMobileRecharge");
+    By Promotion_Banner = MobileBy.id("rvPromotion");
+    By ViewAll_Button = MobileBy.id("btnPromotionViewAll");
+    By History_Button = MobileBy.id("menuHistory");
+    By Voucher_Button = MobileBy.id("menuVoucher");
+    By Profile_Button = MobileBy.id("menuProfile");
+//    By Warning_Message = MobileBy.id();
+}
--- src/main/java/demo/pages/PaymentByHistory.java
+++ src/main/java/demo/pages/PaymentByHistory.java
@@ -6,12 +6,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageobjects.AndroidPageObject;
 
 public class PaymentByHistory {
+    AndroidPageObject pageObject = new AndroidPageObject();
+
     public static boolean isOnPage() {
         WebDriverWait wait = new WebDriverWait(androidDriver, 30);
         WebElement signInLabel = wait
                 .until(ExpectedConditions.presenceOfElementLocated(By.id("alreadySignInTextView")));
         return signInLabel.isDisplayed();
+    }
+
+    public void chooseHistory(){pageObject.clickOn(History_Button);
     }
 }
