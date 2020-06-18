dd01e5e3107139ca00e944112db66bfdcb7b8145
Nauval Shafiq <nauval.shafiq@dana.id>
2020-06-18 17:48:12 +0700

    adding new locator

---
+++ src/main/java/demo/locator/nauval/HistoryPageLocator.java
@@ -0,0 +1,23 @@
+package demo.locator.nauval;
+
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.By;
+
+public interface HistoryPageLocator {
+    // Text View
+    By RECENT_PHONE_NUMBER = MobileBy.id("tvRecentNumberPhone");
+    By RECENT_DATE = MobileBy.id("tvRecentNumberDate");
+
+    // Image View
+    By RECENT_PROVIDER_ICON = MobileBy.id("ivRecentNumberProvider");
+
+    // Edit Text
+    By INPUT_PHONE_NUMBER = MobileBy.id("edtPhoneNumber");
+
+    // Button
+    By SELECT_CONTACT = MobileBy.id("btnSelectContact");
+    By BACK_BUTTON = MobileBy.id("btnBack");
+
+    // Recycle View
+    By RECENT_NUMBER_ITEMS = MobileBy.id("rvRecentNumber");
+}
