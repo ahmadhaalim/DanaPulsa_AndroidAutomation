addb82a9bb96794c0748c813bfe16c654315b4b8
Jonathan Theodoric <theodoricjonathan@gmail.com>
2020-06-25 15:33:09 +0700

    Added driver

--- src/main/java/demo/driver/AndroidDriverInstance.java
+++ src/main/java/demo/driver/AndroidDriverInstance.java
@@ -22,8 +22,12 @@ public class AndroidDriverInstance {
         caps.setCapability("autoGrantPermissions", true);
         caps.setCapability("newCommandTimeout", 120);
         caps.setCapability("app",
-                System.getProperty("user.dir") + File.separator + "APP" + File.separator + "paystore v1.4.apk");
+                System.getProperty("user.dir") + File.separator + "APP" + File.separator + "DANA-Pulsa-1.0.2-develop-debug.apk");
         caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appPackage", "id.dana.apprentech.danapulsa.develop");
+        caps.setCapability("appActivity", "id.dana.apprentech.danapulsa.ui.splash.SplashActivity");
+        caps.setCapability("skipDeviceInitialization", true);
+        caps.setCapability("skipServerInstallation", true);
         try {
             androidDriver = new AndroidDriver<>(new URL(appiumUrl), caps);
             androidDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
